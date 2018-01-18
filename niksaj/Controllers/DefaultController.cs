b765009d1b1caf88b2f5b0f29895b1e8336e363f
sajid <sajidahmed.77@gmail.com>
2018-01-18 12:26:05 +0300

    d

--- niksaj/Controllers/DefaultController.cs
+++ niksaj/Controllers/DefaultController.cs
@@ -11,6 +11,7 @@ namespace niksaj.Controllers
         // GET: Default
         public ActionResult Index()
         {
+            string s = "";
             string strName = "N";
             return View();
         }
